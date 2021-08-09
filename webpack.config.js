ad83418bfebc465df5e97e358fe4e452d5584550
Pedro Manoel <dev.pedromanoel@pm.me>
2021-08-09 14:05:48 -0300

    fix(path)🐛: Fix the index.js path
    There was a error to find src/index.js because the './' was missing.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: ['@babel/polyfill', 'whatwg-fetch', 'src/index.js'],
+  entry: ['@babel/polyfill', 'whatwg-fetch', './src/index.js'],
   output: {
     path: path.resolve(__dirname, 'public/'),
     filename: 'app.js',
