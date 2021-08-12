9c6f7c34edfcaf9bb493d041324d29edb26e668b
Pedro Manoel <dev.pedromanoel@pm.me>
2021-08-12 16:34:21 -0300

    chore🗑 : Take off the garbage
    Remove unnecessary code lines

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import handleInput from './components/handleInput.js';
-// import fetchArticle from './components/fetch-article.js';
 
 handleInput();
 // fetchArticle('../src/assets/article.html');
